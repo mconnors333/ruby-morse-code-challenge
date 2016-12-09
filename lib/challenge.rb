f57a4af3d61ba9a16cf6952a5ef63529c7870e71
mconnors333 <matthew.connors333@gmail.com>
2016-12-09 09:00:37 -0500

    Connors Week04 Day 04 Challenge

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -16,8 +16,31 @@ MORSE_CODE = {
 ## DO NOT CHANGE ANYTHING ABOVE THIS LINE
 
 def decode_morse(morse_code)
-  # Your code here
+  morse_code.split("   ").map do |e|
+    e.split(" ").map { |e| MORSE_CODE[e] || '*' }.join
+  end.join(" ")
 end
+  # Your code here
+  # words = morse_code.split("   ")
+
+  # characters = []
+  #
+  # words.each do |word|
+  #   characters.push(word.split(" "))
+  # end
+  #
+  # word_array = []
+  #
+  # characters.each do |character|
+  #   MORSE_CODE.each do |code, letter|
+  #     if character == code
+  #       print letter
+  #     end
+  #   end
+  # end
+# end
+
+decode_morse('... --- ...   ... --- ...')
 
 def parse_bits(bits)
   # Your code here
